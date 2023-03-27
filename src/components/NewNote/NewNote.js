5ed28e7bafdc40f2b86c002e7e422ec06bb683fa
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-03-27 17:41:38 +0200

    add css modules classes

--- src/components/NewNote/NewNote.js
+++ src/components/NewNote/NewNote.js
@@ -1,52 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+import clasess from "./NewNote.module.css";
 
 function NewNote(props) {
+	const [showForm, setShowForm] = useState(false);
+	const [title, setTitle] = useState("");
+	const [desc, setDesc] = useState("");
 
-    const [showForm, setShowForm] = useState(false)
-    const [title, setTitle] = useState('');
-    const [desc, setDesc] = useState('');
+	function changeTitleHandler(event) {
+		const value = event.target.value;
+		setTitle(value);
+	}
 
-    function changeTitleHandler(event) {
-        const value = event.target.value;
-        setTitle(value);
-    }
+	function changeDescHandler(event) {
+		const value = event.target.value;
+		setDesc(value);
+	}
 
-    function changeDescHandler(event) {
-        const value = event.target.value;
-        setDesc(value);
-    }
+	function addNote() {
+		const note = {
+			title: title,
+			body: desc,
+		};
+		props.onAdd(note);
 
-    function addNote() {
-        const note = {
-            title: title,
-            body: desc,
-        };
-        props.onAdd(note);
+		setTitle("");
+		setDesc("");
+		setShowForm(false);
+	}
 
-        setTitle('');
-        setDesc('');
-        setShowForm(false);
-    }
+	return showForm ? (
+		<div className={clasess["new-note"]}>
+			<label>Tytuł:</label>
+			<input type="text" value={title} onChange={changeTitleHandler} />
 
-    return (
-        showForm ? (
-            <div className="note">
-                <label>Tytuł:</label>
-                <input type="text"
-                    value={title}
-                    onChange={changeTitleHandler} />
+			<label>Opis:</label>
+			<input type="text" value={desc} onChange={changeDescHandler} />
 
-                <label>Opis:</label>
-                <input type="text"
-                    value={desc}
-                    onChange={changeDescHandler} />
-
-                <button onClick={() => addNote()}>Dodaj notatkę</button>
-            </div>
-        ) : (
-            <button className="note" onClick={() => setShowForm(true)}>Nowa notatka</button>
-        )
-    );
-};
+			<button onClick={() => addNote()}>Dodaj notatkę</button>
+		</div>
+	) : (
+		<button onClick={() => setShowForm(true)}>Nowa notatka</button>
+	);
+}
 
 export default NewNote;
