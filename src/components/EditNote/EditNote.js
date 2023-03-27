6952439acffeca5b82240fb637cbfa9fb36cd1d7
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-03-27 17:41:21 +0200

    add classes css modules

--- src/components/EditNote/EditNote.js
+++ src/components/EditNote/EditNote.js
@@ -1,44 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+import classes from "./EditNote.module.css";
 
 function EditNote(props) {
+	const [title, setTitle] = useState(props.title);
+	const [desc, setDesc] = useState(props.body);
 
-    const [title, setTitle] = useState(props.title);
-    const [desc, setDesc] = useState(props.body);
+	function changeTitleHandler(event) {
+		const value = event.target.value;
+		setTitle(value);
+	}
 
-    function changeTitleHandler(event) {
-        const value = event.target.value;
-        setTitle(value);
-    }
+	function changeDescHandler(event) {
+		const value = event.target.value;
+		setDesc(value);
+	}
 
-    function changeDescHandler(event) {
-        const value = event.target.value;
-        setDesc(value);
-    }
+	function editNote() {
+		const note = {
+			title: title,
+			body: desc,
+			id: props.id,
+		};
+		props.onEdit(note);
+	}
 
-    function editNote() {
-        const note = {
-            title: title,
-            body: desc,
-            id: props.id,
-        };
-        props.onEdit(note);
-    }
+	return (
+		<div className={classes["edit-note"]}>
+			<label>Tytuł:</label>
+			<input type="text" value={title} onChange={changeTitleHandler} />
 
-    return (
-        <div className="note">
-            <label>Tytuł:</label>
-            <input type="text"
-                value={title}
-                onChange={changeTitleHandler} />
+			<label>Opis:</label>
+			<input type="text" value={desc} onChange={changeDescHandler} />
 
-            <label>Opis:</label>
-            <input type="text"
-                value={desc}
-                onChange={changeDescHandler} />
-
-            <button onClick={() => editNote()}>Zapisz notatkę</button>
-        </div>
-    );
-};
+			<button onClick={() => editNote()}>Zapisz notatkę</button>
+		</div>
+	);
+}
 
 export default EditNote;
