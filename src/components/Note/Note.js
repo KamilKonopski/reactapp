625059c4cca503784c7b6241ba359f7faa79e925
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-03-27 17:41:00 +0200

    add classes css modules

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,33 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+import classes from "./Note.module.css";
 
 function Note(props) {
+	const [showDesc, setShowDesc] = useState(false);
 
-    const [showDesc, setShowDesc] = useState(false);
+	function toggleDesc() {
+		setShowDesc(!showDesc);
+	}
 
-    function toggleDesc() {
-        setShowDesc(!showDesc);
-    }
+	function editHandler() {
+		props.onEdit({
+			title: props.title,
+			body: props.body,
+			id: props.id,
+		});
+	}
 
-    function editHandler() {
-        props.onEdit({
-            title: props.title,
-            body: props.body,
-            id: props.id,
-        });
-    }
-
-    return (
-        <div className="note">
-            <h2 onClick={toggleDesc}>{props.title}</h2>
-            {showDesc && (
-                <p className="description">{props.body}</p>
-            )}
-            <button onClick={editHandler}>Edytuj</button>
-            <button
-                className="delete"
-                onClick={() => props.onDelete(props.id)}>Usuń</button>
-        </div>
-    );
-};
+	return (
+		<div onClick={toggleDesc} className={classes.note}>
+			<h2>{props.title}</h2>
+			{showDesc && <p className="description">{props.body}</p>}
+			<button onClick={editHandler}>Edytuj</button>
+			<button
+				className={classes.delete}
+				onClick={() => props.onDelete(props.id)}
+			>
+				Usuń
+			</button>
+		</div>
+	);
+}
 
 export default Note;
